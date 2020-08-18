05d1abec0214251f49d0a17dbffe62de09a6849c
조경진 <kj_cho@idealidea.co.kr>
2020-08-18 19:57:10 +0900

    프래그먼트에서 토스트 띄우기   - 화면 정보로 activity 변수 활용

--- app/src/main/java/kr/co/tjoeun/fragment_20200817/fragments/SecondFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200817/fragments/SecondFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
 import kr.co.tjoeun.fragment_20200817.R
 
 class SecondFragment : Fragment() {
@@ -19,6 +21,10 @@ class SecondFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener {
+            Toast.makeText(activity, "토스트 출력", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
